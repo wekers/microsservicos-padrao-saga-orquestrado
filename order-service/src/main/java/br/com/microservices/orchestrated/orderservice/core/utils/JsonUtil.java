38838146a9b9304ee72db9adb21476c11e5d8cfb
wekers <fernando@wekers.org>
2024-11-27 18:09:16 -0300

    change private to public

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/utils/JsonUtil.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/utils/JsonUtil.java
@@ -12,7 +12,7 @@ public class JsonUtil {
 
     private final ObjectMapper objectMapper;
 
-    private String toJson(Object object) {
+    public String toJson(Object object) {
         try {
             return objectMapper.writeValueAsString(object);
         } catch (Exception ex) {
