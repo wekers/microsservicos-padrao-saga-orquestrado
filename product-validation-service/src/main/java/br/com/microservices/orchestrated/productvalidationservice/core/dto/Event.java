0476344d3011bdb1e137de5a1bbb53214efc42a0
wekers <fernando@wekers.org>
2024-12-01 22:06:39 -0300

    add method -> addToHistory

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/Event.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/Event.java
@@ -7,8 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.util.ObjectUtils.isEmpty;
 @Data
 @Builder
 @NoArgsConstructor
@@ -23,4 +25,11 @@ public class Event {
     private String source;
     private List<History> eventHistory;
     private LocalDateTime createdAt;
+
+    public void addToHistory(History history) {
+        if(isEmpty(eventHistory)){
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
