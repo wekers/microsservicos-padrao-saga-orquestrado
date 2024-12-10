78c9fd68025577e5f3435e2c8edc54402d231905
wekers <fernando@wekers.org>
2024-12-09 23:49:33 -0300

    Inventory-Service -> logic to create order to update

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -0,0 +1,77 @@
+package br.com.microservices.orchestrated.inventoryservice.core.service;
+
+
+import br.com.microservices.orchestrated.inventoryservice.config.exception.ValidationException;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.inventoryservice.core.model.Inventory;
+import br.com.microservices.orchestrated.inventoryservice.core.model.OrderInventory;
+import br.com.microservices.orchestrated.inventoryservice.core.producer.KafkaProducer;
+import br.com.microservices.orchestrated.inventoryservice.core.repository.InventoryRepository;
+import br.com.microservices.orchestrated.inventoryservice.core.repository.OrderInventoryRepository;
+import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class InventoryService {
+
+    private static final String CURRENT_SOURCE = "INVENTORY_SERVICE";
+
+    private final JsonUtil jsonUtil;
+    private final KafkaProducer producer;
+    private final InventoryRepository inventoryRepository;
+    private final OrderInventoryRepository orderInventoryRepository;
+
+
+    public void updateInventory(Event event) {
+        try {
+            checkCurrentValidation(event);
+            createOrderInventory(event);
+        } catch (Exception ex) {
+            log.error("Error try update inventory", ex);
+        }
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private Inventory findInventoryByProductCode(String productCode) {
+        return inventoryRepository
+                .findByProductCode(productCode)
+                .orElseThrow(() -> new ValidationException("Inventory not found by informed product code: " + productCode));
+
+    }
+
+    private void createOrderInventory(Event event) {
+        event.
+                getPayload()
+                .getProducts()
+                .forEach(product -> {
+                    var inventory = findInventoryByProductCode(product.getProduct().getCode());
+                    var orderInventory = createOrderInventory(event, product, inventory);
+                    orderInventoryRepository.save(orderInventory);
+                });
+    }
+
+    private OrderInventory createOrderInventory(Event event, OrderProducts product, Inventory inventory) {
+        return OrderInventory
+                .builder()
+                .inventory(inventory)
+                .oldQuantity(inventory.getAvailable())
+                .orderQuantity(product.getQuantity())
+                .newQuantity(inventory.getAvailable() - product.getQuantity())
+                .orderId(event.getPayload().getId())
+                .transactionId(event.getTransactionId())
+                .build();
+
+    }
+
+    private void checkCurrentValidation(Event event) {
+        if (orderInventoryRepository.existsByOrderIdAndTransactionId(event.getPayload().getId(), event.getTransactionId())) {
+            throw new ValidationException("There's another transactionId for this validation.");
+        }
+
+    }
+}
